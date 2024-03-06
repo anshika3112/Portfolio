337c0177a135fdf2b30d42fccce8363a04d25f3c
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-03-06 19:03:58 +0530

    Add files via upload

---
+++ src/assets/icons/index.js
@@ -0,0 +1,47 @@
+import css from './css.svg'
+import git from './git.svg'
+import github from './github.svg'
+import c from './c.svg'
+import calculator from './calculator.svg'
+import python from './python.svg'
+import cpp from './cpp.svg'
+import html from './html.svg'
+import plant from './plant.png'
+import react from './react.svg'
+import javascript from './javascript.svg'
+import nodejs from './nodejs.svg'
+import tailwindcss from './tailwindcss.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import apple from './apple.svg'
+import codepen from './codepen.svg'
+import emotion from './emotion.png'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+
+export {
+    css,
+    git,
+    github,
+    codepen,
+    c,
+    cpp,
+    python,
+    html,
+    javascript,
+    emotion,
+    nodejs,
+    react,
+    tailwindcss,
+    linkedin,
+    twitter,
+    calculator,
+    apple,
+    plant,
+    arrow,
+    contact,
+    soundon,
+    soundoff
+}
